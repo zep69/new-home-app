74a01bd0e5fbcdb0ffa255ec290b4b2b07cb5d41
Nikitos <nikita0609991@gmail.com>
2026-03-30 02:13:07 +0400

    final design update profile and start budget page

--- src/pages/main/Budget.vue
+++ src/pages/main/Budget.vue
@@ -1,6 +1,26 @@
 <template>
-    <div>
-
+    <div class="budget">
+        <div class="budget--total_balance">
+            <span>Ваш баланс</span>
+            <h1>777777,77 ₽</h1>
+        </div>
+        <div class="budget--chart">
+            <h2>Chart block</h2>
+        </div>
+        <div class="budget--actions">
+            <button class="budget--actions--button">
+                <i class="mdi mdi-plus"></i>
+                Добавить траты
+            </button>
+            <button class="budget--actions--button">
+                <i class="mdi mdi-wallet-plus"></i>
+                Добавить прибыль
+            </button>
+            <button class="budget--actions--button">
+                <i class="mdi mdi-cash-fast"></i>
+                Сделать перевод
+            </button>
+        </div>
     </div>
 </template>
 
@@ -9,5 +29,46 @@
 </script>
 
 <style scoped lang="scss">
-
+.budget {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    &--total_balance {
+        display: flex;
+        flex-direction: column;
+    }
+    &--chart {
+        width: 100%;
+        padding: 10px;
+        background: var(--primary-color-t7);
+        margin-top: 10px;
+        border-radius: 15px;
+        height: 150px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    &--actions {
+        display: flex;
+        justify-content: start;
+        overflow: scroll;
+        margin-top: 10px;
+        max-width: 100%;
+        &--button {
+            border-radius: 15px;
+            padding: 10px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-right: 5px;
+            max-height: 45px;
+            white-space: nowrap;
+            border: none;
+            &--expense {
+                color: var(--primary-color);
+                background-color: var(--primary);
+            }
+        }
+    }
+}
 </style>
--- src/pages/main/Profile.vue
+++ src/pages/main/Profile.vue
@@ -19,10 +19,45 @@
         <div class="profile--settings">
             <span class="">Настройки аккаунта</span>
             <div class="profile--settings--block">
-
+                <div class="profile--settings--block--item">
+                    <div>
+                        <i class="mdi mdi-account profile--settings--block--item--icon"></i>
+                        <span class="profile--settings--block--item--text">Персональная информация</span>
+                    </div>
+                    
+                    <i class="mdi mdi-arrow-right"></i>
+                </div>
+                <div class="profile--settings--block--item">
+                    <div>
+                        <i class="mdi mdi-security profile--settings--block--item--icon"></i>
+                        <span class="profile--settings--block--item--text">Пароль и безопасность</span>
+                    </div>
+                    
+                    <i class="mdi mdi-arrow-right"></i>
+                </div>
             </div>
         </div>
-        
+        <div class="profile--members">
+            <span>Участники семьи</span>
+            <div class="profile--members--block">
+                <div class="profile--members--block--item" v-for="item in members">
+                    <div >
+                        <img class="profile--members--block--item--avatar" :src="item.avatar" alt="">
+                    </div>
+                    
+                </div>
+                <div class="profile--members--block--add">
+                    <i class="mdi mdi-plus"></i>
+                </div>
+            </div>
+
+        </div>
+        <div class="profile--logout">
+            <button class="profile--logout--button">
+                <i class="mdi mdi-logout"></i>
+                Выйти
+            </button>
+        </div>
     </div>
 </template>
 
@@ -36,6 +71,22 @@ type Module = {
     icon : string,
 }
 
+type Member = {
+    name:string,
+    avatar:string
+}
+
+
+const members = ref<Member[]>([
+    {
+        name:'Test sets',
+        avatar:'https://media.istockphoto.com/id/2212478701/vector/monochrome-female-silhouette-with-short-hair.jpg?s=612x612&w=0&k=20&c=Q_6Z6fOgbLwV3lBeCOYybXNNkkeTj8bjUMlIcy8aGtI='
+    },
+    {
+        name:'Test sets',
+        avatar:'https://media.istockphoto.com/id/2212478701/vector/monochrome-female-silhouette-with-short-hair.jpg?s=612x612&w=0&k=20&c=Q_6Z6fOgbLwV3lBeCOYybXNNkkeTj8bjUMlIcy8aGtI='
+    }
+])
 
 const modules = ref<Module[]>([
     {
@@ -104,6 +155,75 @@ const modules = ref<Module[]>([
             border-radius: 15px;
             padding: 15px;
             margin-top: 10px;
+            &--item {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                background: none;
+                padding: 5px;
+                margin-bottom: 5px;
+                &--text {
+                    font-size: 18px;
+                    font-style: bold;
+                    margin-left: 10px;
+                    color: var(--primary-color);
+                }
+                &--icon {
+                    font-size: 24px;
+                    padding: 5px;
+                    background-color: var(--bg-color);
+                    border-radius: 5px;
+                    color: var(--primary-color);
+                }
+            }
+
+        }
+    }
+    &--members {
+        margin-top: 10px;
+        width: 100%;
+        &--block {
+            display: flex;
+            justify-content: start;
+            align-items: center;
+            margin-top: 10px;
+            &--item {
+                border-radius: 50%;
+                padding: 4px;
+                border: solid thin var(--primary-color);
+                height: 70px;
+                width: 70px;
+                margin-right: 10px;
+                &--avatar {
+                    height: 60px;
+                    width: 60px;
+                    border-radius: 50%;
+                }
+            }
+            &--add {
+                height: 70px;
+                width: 70px;
+                border-radius: 50%;
+                border:thin dashed var(--primary-color);
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                color: var(--primary-color);
+                font-size: 30px;
+            }
+        }
+    }
+    &--logout {
+        margin-top: 10px;
+        width: 100%;
+        &--button {
+            background: none;
+            border: 2px solid var(--error-color-tonal);
+            color: var(--error-color);
+            width: 100%;
+            padding: 15px;
+            border-radius: 15px;
+            font-size: 24px;
         }
     }
 }
